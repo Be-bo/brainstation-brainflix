7f6223deb007c9ee6ceeac8f656cca7c617c5367
Be-bo <robfiker@gmail.com>
2023-10-23 13:21:12 -0700

    Buildout and responsiveness styling of the Upload page finished

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import "./Upload.scss";
-import thumbnail from '../../assets/Images/Upload-video-preview.jpg';
+import thumbnail from "../../assets/Images/Upload-video-preview.jpg";
 
 function Upload() {
 	return (
@@ -10,22 +10,48 @@ function Upload() {
 			<NavBar />
 
 			<form className="upload-form" action="submit">
-				<h1 className="upload-form__title">Upload Video</h1>
-				<h3 className="upload-form__thumbnail-title upload-form__title upload-form__label">Video Thumbnail</h3>
-				<img className="upload-form__thumbnail" src={thumbnail}/>
+				<h1 className="upload-form__page-title upload-form__title">Upload Video</h1>
 
-				<h3 className="upload-form__video-title upload-form__title upload-form__label">Title Your Video</h3>
-				<input className="upload-form__name" type="text" placeholder="Add a title to your video" />
+				<div className="upload-form__input-layer">
+					<div className="upload-form__thumbnail-div">
+						<h3 className="upload-form__title upload-form__label">
+							Video Thumbnail
+						</h3>
+						<img className="upload-form__thumbnail" src={thumbnail} />
+					</div>
 
-				<h3 className="upload-form__description-title upload-form__title upload-form__label">Add a Video Description</h3>
-				<textarea className="upload-form__description" placeholder="Add a description to your video" />
+					<div className="upload-form__title-desc-div">
+						<h3 className="upload-form__video-title upload-form__title upload-form__label">
+							Title Your Video
+						</h3>
+						<input
+							className="upload-form__name"
+							type="text"
+							placeholder="Add a title to your video"
+						/>
 
-				<div className="upload-form__btn-container">
-					<i className="upload-form__btn-icon"/>
-					<button className="upload-form__btn" type="submit">Publish</button>
+						<h3 className="upload-form__description-title upload-form__title upload-form__label">
+							Add a Video Description
+						</h3>
+						<textarea
+							className="upload-form__description"
+							placeholder="Add a description to your video"
+						/>
+					</div>
 				</div>
 
-				<Link className="upload-form__cancel-btn" to='/'>Cancel</Link>
+				<div className="upload-form__btn-div">
+					<div className="upload-form__btn-container">
+						<i className="upload-form__btn-icon" />
+						<button className="upload-form__btn" type="submit">
+							Publish
+						</button>
+					</div>
+
+					<Link className="upload-form__cancel-btn" to="/">
+						Cancel
+					</Link>
+				</div>
 			</form>
 		</div>
 	);
